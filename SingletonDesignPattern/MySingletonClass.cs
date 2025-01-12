7e2b25b05ffe889c49941ed9b86a0126308f67da
sumit <sumit@DESKTOP-4KB4ML7>
2025-01-12 15:30:29 +0530

    Add project files.

---
+++ SingletonDesignPattern/MySingletonClass.cs
@@ -0,0 +1,34 @@
+﻿namespace SingletonDesignPattern
+{
+	sealed class MySingletonClass
+	{
+		private MySingletonClass()
+		{
+
+		}
+
+		private static MySingletonClass instance;
+		private static object instanceLock = new object();
+		public static MySingletonClass GetInstance()
+		{
+			if (instance == null)
+			{
+				// Below checking multithreding scenario
+				lock (instanceLock)
+				{
+					if (instance == null)
+					{
+						instance = new MySingletonClass();
+					}
+				}
+
+			}
+
+
+			return instance;
+
+
+		}
+
+	}
+}
---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SingletonDesignPattern
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+
+			MySingletonClass singletonObj1 = MySingletonClass.GetInstance();
+			MySingletonClass singletonObj2 = MySingletonClass.GetInstance();
+
+			//above both singletonObj1 & singletonObj2 will get same instance of MySingletonClass 
+
+		}
+	}
+}
