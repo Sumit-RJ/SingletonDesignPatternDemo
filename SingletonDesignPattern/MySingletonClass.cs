d280e356d6705a6fdb6407ad704628578560e6e0
sumit <sumit@DESKTOP-4KB4ML7>
2025-01-12 15:32:26 +0530

    Comments added

--- SingletonDesignPattern/MySingletonClass.cs
+++ SingletonDesignPattern/MySingletonClass.cs
@@ -1,7 +1,9 @@
 ﻿namespace SingletonDesignPattern
 {
+	//sealed keyword will prevent inheritance
 	sealed class MySingletonClass
 	{
+		//private constructor will prevent object creation of the class
 		private MySingletonClass()
 		{
 
@@ -9,6 +11,11 @@
 
 		private static MySingletonClass instance;
 		private static object instanceLock = new object();
+
+		/// <summary>
+		/// Global entry point 
+		/// </summary>
+		/// <returns>singleton object of class</returns>
 		public static MySingletonClass GetInstance()
 		{
 			if (instance == null)
